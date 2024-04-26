97b1e212c4bbbff29d57c192400a8bf49c9edaef
gryzlegrizz <gryzlegrizz@gmail.com>
2024-04-26 16:01:48 +0700

    update get.js

--- js/fetch.js
+++ js/fetch.js
@@ -1,26 +1,3 @@
-
-// import { get } from "https://bukulapak.github.io/api/process.js";
-// import { addInner } from "https://bukulapak.github.io/element/process.js";
-// import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-// import { isiTabel } from "./temp/table.js";
-// let urlAPI = "https://ws-gilang2024-1716ba1b2ba6.herokuapp.com/presensi";
-// get(urlAPI, isiTablePresensi);
-// function isiTablePresensi(results) {
-//     results.forEach(isiRow);
-// }
-// function isiRow(value) {
-//     let content = 
-//     isiTabel.replace("#NAMA#", value.biodata.nama)
-//             .replace("#NOHP#", value.biodata.phone_number)
-//             .replace("#JABATAN#", value.biodata.jabatan)
-//             .replace("#LOKASI#", value.location)
-//             .replace("#STATUS#", value.checkin)
-//             .replace("#HARIKERJA#", value.biodata.hari_kerja)
-//             .replace("#WARNA#", getRandomColor())
-//             .replace(/#WARNALOGO#/g, getRandomColorName());
-//         addInner("iniTabel", content);
-// }
-
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { isiTablePresensi } from "./controller/get.js";
 import { urlAPI } from "./config/url.js";
